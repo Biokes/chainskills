d971086633eb848d7b9a174f3ca0e3e70739234a
Biokes <106444765+Biokes@users.noreply.github.com>
2025-10-28 22:04:46 +0100

    update: refactor

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
-import { AppProvider } from './providers/HederaAppProvider.tsx';
 import { Toaster } from "@/components/ui/sonner";
+import { AppProvider } from './providers/HederaAppProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
